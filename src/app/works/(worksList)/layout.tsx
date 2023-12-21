a7d8902af194f59aceb620db139d24a8ae7a7739
Yongseung Song <yongseung.song@icloud.com>
2023-12-22 02:36:03 +0900

    initiate Project

--- src/app/works/(worksList)/layout.tsx
+++ src/app/works/(worksList)/layout.tsx
@@ -3,7 +3,7 @@ type Children = React.ReactElement;
 const DetailPageLayout = ({ children }: { children: Children }) => {
   return (
     <div className="min-h-screen">
-      <h1>layout from parent</h1>
+      <h1>layout from Parent</h1>
       <div>{children}</div>
     </div>
   );
